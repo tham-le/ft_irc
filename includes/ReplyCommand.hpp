9c7113fd4c1d604f3a61bc2a468c55b46982fb99
Yulia BOKTAEVA <yuboktae@bess-f4r3s14.clusters.42paris.fr>
2024-01-17 20:50:37 +0100

    update1 RPL for command.hpp

--- includes/ReplyCommand.hpp
+++ includes/ReplyCommand.hpp
@@ -2,33 +2,40 @@
 # define REPLYCOMMAND_HPP
 
 /*ERROR FOR COMMAND*/
-# define ERR_INPUTTOOLONG " :Input line too long\r\n" // 417
-# define ERR_NONICKNAMEGIVEN() (":No nickname given\r\n") // 431
-# define ERR_ERRONEUSNICKNAME(nick) (nick + " : Erroneus nickname\r\n") // 432
-# define ERR_NICKNAMEINUSE(nick) (nick + " : Nickname is already in use\r\n") // 433
-# define ERR_NICKCOLLISION " : Nickname collision KILL\r\n" // 436
-# define ERR_NOSUCHNICK(name) (name + " :No such nick/channel\r\n") // 401
-# define ERR_UNKNOWNCOMMAND(command) (command + " :Unknown command\r\n") // 421
-# define ERR_NOSUCHCHANNEL " :No such channel\r\n" // 403
-# define ERR_NOCHANNNELS " :No channel created\r\n"
-# define ERR_USERNOTINCHANNEL " :They aren't on that channel\r\n" // 441 quand on veut kick ou change mode d'un user qui n'est pas dans le channel
-# define ERR_NOTONCHANNEL " :You're not on that channel\r\n" // 442
-# define ERR_USERONCHANNEL " :is already on channel\r\n" // 443
-# define ERR_CHANNELISFULL(channel) (channel + " :Cannot join channel (+l)\r\n") // 471
-# define ERR_INVITEONLYCHAN(channel) (channel + " :Cannot join channel (+i)\r\n") // 473
-# define ERR_UMODEKNOWNFLAG " :Unknown MODE flag\r\n" // 501
-# define ERR_INVALIDMODEPARAM " :Invalid MODE parameter\r\n" // 696
-# define ERR_NEEDMOREPARAMS(command) (command + " :Not enough parameters\r\n") // 461
-# define ERR_BADCHANNELKEY(channel) (channel + " :Cannot join channel (+k)")// 475
+# define ERR_INPUTTOOLONG() ("417" + " :Input line too long\r\n") // 417
+# define ERR_NONICKNAMEGIVEN() ("431 :No nickname given\r\n") // 431
+# define ERR_ERRONEUSNICKNAME(nick) ("432 " + nick + " : Erroneus nickname\r\n") // 432
+# define ERR_NICKNAMEINUSE(nick) ("433 " + nick + " : Nickname is already in use\r\n") // 433
+# define ERR_NICKCOLLISION(nick) ("436 " + nick + " : Nickname collision KILL\r\n") // 436
+# define ERR_NOSUCHNICK(name) ("401 " + name + " :No such nick/channel\r\n") // 401
+# define ERR_UNKNOWNCOMMAND(command) ("421 " + command + " :Unknown command\r\n") // 421
+# define ERR_NOSUCHCHANNEL(channel) ("403 " + channel + " :No such channel\r\n") // 403
+# define ERR_NOCHANNNELS() (" :No channel created\r\n") //notre propre message d'erreur, n'existe pas dans le doc d'IRC
+# define ERR_USERNOTINCHANNEL(nick, channel) ("441 " + nick + " " + channel + " :They aren't on that channel\r\n") // 441 quand on veut kick ou change mode d'un user qui n'est pas dans le channel
+# define ERR_NOTONCHANNEL(channel) ("442 " + channel + " :You're not on that channel\r\n") // 442
+# define ERR_USERONCHANNEL(nick, channel) ("443 " + nick + " " + channel + " :is already on channel\r\n") // 443
+# define ERR_CHANNELISFULL(channel) ("471 " + channel + " :Cannot join channel (+l)\r\n") // 471
+# define ERR_INVITEONLYCHAN(channel) ("473 " + channel + " :Cannot join channel (You must be invited)(+i)\r\n") // 473
+# define ERR_UMODEKNOWNFLAG() ("501" + " :Unknown MODE flag\r\n") // 501
+# define ERR_INVALIDMODEPARAM() ("696" + " :Invalid MODE parameter\r\n") // 696
+# define ERR_NEEDMOREPARAMS(command) ("461 " + command + " :Not enough parameters\r\n") // 461
+# define ERR_NOTJOINEDANYCHANNEL() (" :Not joined to any channel\r\n") //notre propre message d'erreur, n'existe pas dans le doc d'IRC
+# define ERR_BADCHANNELKEY(channel) ("475 " + channel + " :Cannot join channel (+k)")// 475
 
 
 /*REPLY FOR COMMAND*/
-# define RPL_WELCOME(nick) (":Welcome to the Internet Relay Network " + nick + "\r\n")
-# define RPL_YOURHOST(servername, version) (":Your host is " + servername + ", running version " + version + "\r\n")
-# define RPL_MYINFO(servername, version, usr_modes, chann_modes) (":" + servername + " " + version + " " + usr_modes + " " + chann_modes + "\r\n")
-# define RPL_CREATED(date) (":This server was created " + date + "\r\n");
-# define RPL_BOUNCE(server_name, port) ("Try server " + server_name + ", port " + port + "\r\n")
-# define RPL_TOPIC(channel, subject) ("Topic for #" + channel + ": " + subject + "\r\n") // 332
-# define RPL_TOPICWHOTIME(user, time) ("Topic set by " + user + " [" + time + "]")// 333
+# define RPL_WELCOME(nick) ("001 " + ":Welcome to the Internet Relay Network " + nick + "\r\n") //001
+# define RPL_YOURHOST(servername, version) ("002 " + ":Your host is " + servername + ", running version " + version + "\r\n") //002
+# define RPL_MYINFO(servername, version, usr_modes, chann_modes) ("004 " + ":" + servername + " " + version + " " + usr_modes + " " + chann_modes + "\r\n") //004
+# define RPL_CREATED(date) ("003 " + ":This server was created " + date + "\r\n"); //003
+# define RPL_BOUNCE(server_name, port) ("010 " + "Try server " + server_name + ", port " + port + "\r\n") //010
+# define RPL_NICK(nick) ("You're now known as: " + nick + "\r\n") //notre propre message pour le changement de nickname
+# define RPL_LISTSTART() ("321 Channel :Users Name\r\n") //321
+# define RPL_LIST(channel, nb_users, topic) ("322 " + channel + " " + nb_users + " :" + topic + "\r\n") //322
+# define RPL_LISTEND() ("323 :End of /LIST\r\n") //323
+# define RPL_CHANNELMODEIS(channel, mode) ("324 " + channel + " " + mode + "\r\n") //324
+# define RPL_CREATIONTIME(channel, date) ("329 " + channel + " " + date + "\r\n") //329
+# define RPL_TOPIC(channel, subject) ("332 Topic for #" + channel + ": " + subject + "\r\n") // 332
+# define RPL_TOPICWHOTIME(user, time) ("333 Topic set by " + user + " [" + time + "]")// 333
 
 #endif
