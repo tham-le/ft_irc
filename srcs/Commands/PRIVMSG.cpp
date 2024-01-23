2385b39b9a2c6f7149d67f4e86f92f4713eae1a1
Yulia BOKTAEVA <yuboktae@paul-f3Ar5s2.clusters.42paris.fr>
2024-01-23 17:57:16 +0100

    PRIVMSG work with /MSG cmd in channel and between users

--- srcs/Commands/PRIVMSG.cpp
+++ srcs/Commands/PRIVMSG.cpp
@@ -2,47 +2,37 @@
 #include "../includes/ReplyCommand.hpp"
 
 void		Command::privmsg(void) {
-
-    if (_input.size() < 3) {
-        _user.printMessage(461, _input[0]); // ERR_NEEDMOREPARAMS
-        return ;
+    std::string msg;
+    bool colon = false;
+    for (unsigned long i = 0; i != _input.size(); i++) {
+        if (_input[i][0] == ':') {
+            colon = true;
+            msg += _input[i].substr(1);
+        }
+        else if (colon)
+            msg += " " + _input[i];
     }
-
-    for (unsigned int i = 1; i < _input.size() - 1; i++) {
+    for (unsigned int i = 1; i < _input.size(); i++) {
         std::string target = _input[i];
-        if (target[0] == '@' && target[1] == '%' && target[2] == '#') // @%#channel to send msg to opers on channel
-            target = target.substr(3);
-        if (!_ircserv.isUser(target) && !_ircserv.isChannel(target)) {
+        if (!_ircserv.isUser(_input[1]) && !_ircserv.isChannel(_input[1])) {
                 _user.printMessage(401, target); // ERR_NOSUCHNICK
                 return ;
-            }
-            else {
-                _user.printMessage(411, _input[0]); // ERR_NORECIPIENT
-                return ;
-            }
-        if (_input[_input.size() - 1][0] != ':') {
-            _user.printMessage(412); // ERR_NOTEXTTOSEND
-            return ;
         }
         else if (_ircserv.isUser(target)) {
             if (_user.getStatus() == User::ONLINE) {
                 User *user = _ircserv.getUser(target);
-                user->printMessage(toFormat("PRIVMSG", _input[_input.size() - 1]));
+                user->printMessage(toFormat("PRIVMSG", user->getNickname(), msg));
             }
             else
-                _user.printMessage(301, target, _input[_input.size() - 1]); // should code for send RPL_AWAY if user is away
+                _user.printMessage(301, target, msg); // should code for send RPL_AWAY if user is away
         }
         else if (_ircserv.isChannel(target)) {
             Channel *channel = _ircserv.getChannel(target);
             std::map<int, User *> listUsers = channel->getUsers();
-            if (_input[i][0] == '@' && _input[i][1] == '%' && _input[i][2] == '#') {
-                for (std::map<int, User *> ::iterator it = listUsers.begin(); it != listUsers.end(); it++)
-                    if (it->second->isOperator())
-                        it->second->printMessage(toFormat("PRIVMSG", _input[_input.size() - 1])); //send msg to opers on channel
-            }
-            else {
-                for (std::map<int, User *> ::iterator it = listUsers.begin(); it != listUsers.end(); it++)
-                    it->second->printMessage(toFormat("PRIVMSG", _input[_input.size() - 1]));  //how print to channel? send for all clients on channel?
+            for (std::map<int, User *> ::iterator it = listUsers.begin(); it != listUsers.end(); it++) {
+                if (_user.getNickname() == it->second->getNickname())
+                    continue ;
+                it->second->printMessage(toFormat("PRIVMSG", target, msg));  //how print to channel? send for all clients on channel?
             }
         }
     }
