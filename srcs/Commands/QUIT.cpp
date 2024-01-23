d39e279a35f6225860abb1e2bf3def2bb5b6fb1b
Yulia BOKTAEVA <yuboktae@paul-f3Ar5s2.clusters.42paris.fr>
2024-01-23 19:45:18 +0100

    QUIT with a reason

--- srcs/Commands/QUIT.cpp
+++ srcs/Commands/QUIT.cpp
@@ -3,6 +3,27 @@
 
 void	Command::quit(void)
 {
+	std::string reason;
+	if (_input.size() > 2) {
+		for (size_t i = 1; i < _input.size(); i++)
+			reason += " " + _input[i];
+	}
+	if (_user.getStatus() == User::ONLINE)
+	{
+		for (std::map<std::string, Channel *>::iterator it = _user._channels.begin(); it != _user._channels.end(); it++)
+		{
+			if (it->second->isUserInChannel(_user))
+				it->second->removeUser(_user);
+			if (it->second->isOperator(_user))
+				it->second->removeOperator(_user);
+			std::string msg = toFormat("QUIT", _ircserv.getChannel(it->first)->getName() + reason);
+			std::map<int, User *> users = _ircserv.getChannel(it->first)->getUsers();
+			for (std::map<int, User *>::iterator it = users.begin(); it != users.end(); it++)
+				(*it->second).printMessage(msg + "\r\n");
+			if (users.size() == 0)
+				_ircserv.removeChannel(it->first);
+		}
+	}
 	for (std::map<std::string, Channel *>::iterator it = _user._channels.begin(); it != _user._channels.end(); it++) {
 		if (std::find(it->second->_operators.begin(), it->second->_operators.end(), &_user) != it->second->_operators.end())
 			it->second->removeOperator(_user);
@@ -10,6 +31,4 @@ void	Command::quit(void)
 	for (std::map<std::string, Channel *>::iterator it = _user._channels.begin(); it != _user._channels.end(); it++)
 		it->second->removeUser(_user);
 	_ircserv.disconnectClient(_user.getFd());
-	//should send a QUIT message to all clients on the channel: for example dan is exiting form network
-	//if there is a reason for quitting, send it to all users on the channel with this reason
 }
