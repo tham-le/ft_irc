f7caea81b5e5441ea67def7af012fbb80ab3e19f
Yulia BOKTAEVA <yuboktae@paul-f3Ar4s2.clusters.42paris.fr>
2024-01-25 19:20:53 +0100

    update names list

--- srcs/Commands/LIST.cpp
+++ srcs/Commands/LIST.cpp
@@ -10,23 +10,20 @@ void	Command::list(void)
 		return ;
 	}
 	_user.printMessage(321);
-	if (_input.size() > 1 && _input[1] != _input.back() && _input[1][0] != '#') {
+	_user.printMessage(323);
+	if (_input.size() > 1 && _input[1][0] != '#') {
 		_user.printMessage(323);
 		return ;
 	}
-	if (_ircserv.getChannels().empty())
-		_user.printMessage(ERR_NOCHANNNELS());
-	else {
-		int idx = 0;
-		if (_input[1][0] == '#')
-			idx = 1;
-		if (!_input[1].empty() && _ircserv.getChannels().find(&_input[1][idx]) != _ircserv.getChannels().end()) {
-			_user.printMessage(322, &_input[1][idx], to_string(_ircserv.getChannel(&_input[1][idx])->_users.size()),
-			_ircserv.getChannel(&_input[1][idx])->getTopic()); // a refaire
-			_user.printMessage(323);
+	if (_ircserv.getChannels().empty() || _input[1].empty())
+		_user.printMessage(323);
+	std::vector<std::string> channels = split(_input[1], ',');
+	for (size_t i = 0; i < channels.size(); i++) {
+		if (_ircserv.isChannel(channels[i])) {
+			_user.printMessage(322, channels[i], to_string(_ircserv.getChannel(channels[i])->getUsers().size()),
+			_ircserv.getChannel(channels[i])->getTopic());
 		}
-		else if (_input[1].empty()) {
+		else 
 			_user.printMessage(323);
-		}
 	}
 }
